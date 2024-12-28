a328e81a59958b2798100cc01772ff3e8735dee7
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2024-12-28 21:20:43 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,33 +1,17 @@
-import math
-
 
 def solution(progresses, speeds):
-    if len(progresses) == 0:
-        return []
-
     answer = []
-    total_time = 0
-    completed_works = 1
+    for p, s in zip(progresses, speeds) :
+        used_time = -((p - 100) // s)
+        if len(answer) == 0 or answer[-1][1] < used_time:
+            answer.append([1, used_time])
+        else :
+            answer[-1][0] += 1
 
-    for p, s in zip(progresses, speeds):
-        time = math.ceil((100 - p) /s)
-        if time > total_time:
-            if total_time != 0:
-                answer.append(completed_works)
-                completed_works = 1
+    return [v[0] for v in answer]
 
-            used_time = time - total_time
-            total_time += used_time
-
-        else:
-            completed_works += 1
-
-    answer.append(completed_works)
-
-    return answer
 
 
 progresses = [93, 30, 55]
 speeds = [1, 30, 5]
-
 print(solution(progresses, speeds))
