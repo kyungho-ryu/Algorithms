4890a69dd0052b055b4d6967c2499fecb556b379
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2024-12-29 17:27:00 +0900

    [level 2] Title: 주식가격, Time: 29.57 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,20 +1,19 @@
-from collections import deque
-
 def solution(prices):
-    temp = deque(prices)
-    answer = []
+    stack = []
+    answer = [0 for _ in range(len(prices))]
 
-    while temp :
-        price = temp.popleft()
-        decreased = False
-        for i, p in enumerate(temp) :
-            if price > p :
-                answer.append(i+1)
-                decreased = True
-                break
+    for i in range(len(prices)) :
+        while stack and prices[stack[-1]] > prices[i] :
+            idx = stack.pop()
+            answer[idx] = i - idx
 
-        if not decreased :
-            answer.append(len(temp))
+
+        stack.append(i)
+
+    while stack :
+        idx = stack.pop()
+        answer[idx] = len(prices) - idx - 1
+
     return answer
 
 print(solution([1, 2, 3, 2, 3])) # [4, 3, 1, 1, 0]
