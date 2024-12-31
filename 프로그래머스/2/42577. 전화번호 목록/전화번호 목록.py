97edda13432d11f5208056a9181d3c0dcd926b70
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2024-12-31 13:22:03 +0900

    [level 2] Title: 전화번호 목록, Time: 631.21 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,17 @@
 def solution(phone_book):
     hash_table = {}
-    for phone_number in phone_book:
-        hash_table[phone_number] = True
-    
-    for phone_number in phone_book : 
-        temp = ""
-        for char in phone_number :
-            temp +=char
-            if temp in hash_table and temp != phone_number :
-                return False
-    
+    for num in phone_book:
+        hash_table[num] = True
 
+    for num in phone_book :
+        temp = ""
+        for char in num :
+            temp +=char
+            if temp in hash_table.keys() and temp != num :
+                return False
     return True
+
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123", "456", "789"]))
+print(solution(["12", "123", "1235", "567", "88"]))
