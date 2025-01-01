8bbd2a69b61257fc57f0d65a8b783b8e217d2ee6
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-01-01 16:19:46 +0900

    [level 2] Title: H-Index, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,11 @@
 def solution(citations):
-    # 논문의 인용 횟수를 내림차순으로 정렬
-    citations.sort(reverse=True)
-    
-    # H-Index 계산
-    for i in range(len(citations)):
-        # i번째 논문까지는 인용 횟수가 (i+1)번 이상인지 확인
-        if citations[i] < i + 1:
-            return i
-    
-    # 모든 논문이 조건을 만족하면 전체 논문 수를 반환
-    return len(citations)
+    temp = sorted(citations, reverse=True)
 
-# 테스트
-print(solution([3, 0, 6, 1, 5]))  # 3
-print(solution([10, 8, 5, 4, 3]))  # 4
-print(solution([25, 8, 5, 3, 3]))  # 3
-print(solution([0, 0, 0, 0]))      # 0
+    for i in range(len(temp)) :
+        if i+1 > temp[i] : return  i
+
+    return len(citations)
+# 과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+# 논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+print(solution([5,5,5,100, 1]))
+
