a134c76abaa2cd663adab59ad0dd01b9b3b58251
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-01-01 15:17:50 +0900

    [level 2] Title: 가장 큰 수, Time: 67.65 ms, Memory: 23.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
 
 ### 성능 요약
 
-메모리: 27.1 MB, 시간: 55.64 ms
+메모리: 23.5 MB, 시간: 67.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 01일 14:57:32
+2025년 01월 01일 15:17:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,17 +1,12 @@
 def solution(numbers):
-    # 숫자를 문자열로 변환
-    temp = [str(num) for num in numbers]
-    
-    # 정렬 기준: 두 문자열을 이어 붙여 비교
-    temp.sort(key=lambda x: x*3, reverse=True)
-    
-    # 모든 숫자를 이어 붙여 결과 생성
-    answer = ''.join(temp)
-    
-    # 0으로만 이루어진 경우 처리
-    return answer if answer[0] != '0' else '0'
+    answer = ''
 
-# 테스트
-print(solution([6, 10, 2]))  # 6210
-print(solution([3, 30, 34, 5, 9]))  # 9534330
-print(solution([0, 0, 0]))  # 0
+    temp = [str(num) for num in numbers]
+    temp = sorted(temp, key=lambda x:x*3, reverse=True)
+    answer = ''.join(temp)
+    return answer if answer[0] !="0" else '0'
+
+# numbers의 원소는 0 이상 1,000 이하입니다.
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
+print(solution([0,0,0]))
