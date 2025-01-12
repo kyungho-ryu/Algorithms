17f3ab14185e19a705a09eeafc4c512d9c2346ea
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-01-12 22:17:23 +0900

    [level 3] Title: 등굣길, Time: 2.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,11 +1,11 @@
 def solution(m, n, puddles):
     # DP 테이블 초기화
     dp = [[0] * (m + 1) for _ in range(n + 1)]
-    dp[1][1] = 1  # 시작점
-    
+    dp[1][1] = 1  #
+
     # 물에 잠긴 지역을 집합으로 변환
     puddles = {(x, y) for x, y in puddles}
-    
+
     # DP 테이블 채우기
     for y in range(1, n + 1):
         for x in range(1, m + 1):
@@ -13,8 +13,8 @@ def solution(m, n, puddles):
                 # 물에 잠긴 곳 또는 시작점은 처리하지 않음
                 continue
             dp[y][x] = (dp[y - 1][x] + dp[y][x - 1]) % 1000000007  # 위 + 왼쪽
-    
     return dp[n][m]  # 도착점의 경로 개수 반환
+
 
 # 테스트
 print(solution(4, 3, [[2, 2]]))  # 출력: 4
