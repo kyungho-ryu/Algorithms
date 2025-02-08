f55111feb30549ba348784fd981799f8badb1dd1
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-02-08 14:11:30 +0900

    [level 2] Title: 카펫, Time: 0.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,18 +1,6 @@
-def solution(brown, yellow):
-    total_tiles = brown + yellow
-
-    for i in range(3, int(total_tiles**0.5)+1) :
-        if total_tiles % i == 0 :
-            j = total_tiles // i
-            if yellow == (i-2) * (j-2) :
-                return [j, i]
-    return []
-
-# 제한사항
-# 갈색 격자의 수 brown은 8 이상 5,000 이하인 자연수입니다.
-# 노란색 격자의 수 yellow는 1 이상 2,000,000 이하인 자연수입니다.
-# 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
-
-print(solution(10, 2)) # [4,3]
-print(solution(8, 1)) # [3,3]
-print(solution(24, 24)) # [8,6]
+def solution(B, Y) :
+    temp = B / 2
+    for i in range(1, int(temp//2)+1) :
+        print(i)
+        if Y == (temp - i - 2) * i :
+            return [int(temp-i), i+2]
