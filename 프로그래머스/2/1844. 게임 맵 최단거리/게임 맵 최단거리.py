c802cb9be58625817c1efa6a6416011d866af94e
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-05-05 16:49:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.63 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,32 +1,35 @@
 from collections import deque
 
+
 def solution(maps):
-    actions = [[-1,0], [1,0], [0, -1], [0, 1]]
-    queue = deque([(0,0,1)])
-    n, m = len(maps), len(maps[0])
-    vistied = [[False] * m for _ in range(n)]
-    vistied[0][0] = True
+    def bfs(maps, pos):
+        visitied = [[False] * len(maps[0]) for _ in range(len(maps))]
+        visitied[pos[0]][pos[1]] = True
 
-    while queue :
-        x,y,count = queue.popleft()
+        queue = deque([[pos[0], pos[1], 0]])
+        while queue:
+            x, y, _sum = queue.popleft()
+            _sum += 1
 
-        if x == n-1 and y == m-1 :
-            return count
+            if y == len(maps)-1 and x == len(maps[0])-1:
+                return _sum
 
-        for dx, dy in actions :
-            nx, ny = x+dx, y + dy
+            if x - 1 >= 0 and not visitied[y][x - 1] and maps[y][x-1] == 1:
+                queue.append([x - 1, y, _sum])
+                visitied[y][x - 1] = True
+            if x + 1 < len(maps[0]) and not visitied[y][x + 1] and maps[y][x+1] == 1:
+                queue.append([x + 1, y, _sum])
+                visitied[y][x + 1] = True
+            if y - 1 >= 0 and not visitied[y - 1][x] and maps[y-1][x] == 1:
+                queue.append([x, y - 1, _sum])
+                visitied[y - 1][x] = True
+            if y + 1 < len(maps) and not visitied[y + 1][x] and maps[y+1][x] == 1:
+                queue.append([x, y + 1, _sum])
+                visitied[y + 1][x] = True
 
-            if 0 <=nx < n and 0<=ny < m and not vistied[nx][ny] and maps[nx][ny] == 1 :
-                vistied[nx][ny] = True
-                queue.append((nx, ny, count+1))
+    result = bfs(maps, (0, 0))
+    return result if result else -1
 
-    return -1
 
-# 제한사항
-# maps는 n x m 크기의 게임 맵의 상태가 들어있는 2차원 배열로, n과 m은 각각 1 이상 100 이하의 자연수입니다.
-# n과 m은 서로 같을 수도, 다를 수도 있지만, n과 m이 모두 1인 경우는 입력으로 주어지지 않습니다.
-# maps는 0과 1로만 이루어져 있으며, 0은 벽이 있는 자리, 1은 벽이 없는 자리를 나타냅니다.
-# 처음에 캐릭터는 게임 맵의 좌측 상단인 (1, 1) 위치에 있으며, 상대방 진영은 게임 맵의 우측 하단인 (n, m) 위치에 있습니다.
-
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]])) # 11
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]])) # -1
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+print(solution(maps))
