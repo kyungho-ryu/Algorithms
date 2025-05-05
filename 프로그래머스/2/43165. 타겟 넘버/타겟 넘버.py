3cb2e8b05f784d48057576d03f8e2ce16b277f55
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-05-05 16:13:14 +0900

    [level 2] Title: 타겟 넘버, Time: 458.79 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,11 @@
-def solution(numbers, target):
-    def dfs(index, sum) :
-        if index == len(numbers):
-            return 1 if sum == target else 0
-
-        return dfs(index+1, sum+numbers[index]) + dfs(index+1, sum-numbers[index])
-
+def solution(numbers, target) :
+    def dfs (index, _sum) :
+        if index == len(numbers) :
+            return 1 if _sum == target else 0 
+        
+        return dfs(index+1, _sum + numbers[index]) + dfs(index + 1 , _sum - numbers[index])
+    
     return dfs(0, 0)
-
-
-print(solution([1, 1, 1, 1, 1], 3))  # 5
-print(solution([4, 1, 2, 1], 4))  # 2
+        
+    
+print(solution([1,1,1,1,1], 3))
