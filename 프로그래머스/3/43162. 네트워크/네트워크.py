41551d051ef6d2b64ba35747e2cb121dab8d020c
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-05-08 22:00:21 +0900

    [level 3] Title: 네트워크, Time: 0.38 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,25 @@
-def solution(n, computers):
-    def dfs(i, visited) :
-        visited[i] = True
-        for j in range(n) :
-            if not visited[j] and computers[i][j] ==1 :
-                dfs(j, visited)
+from collections import deque
 
-    answer = 0
+def solution(n, computers) :
     visited = [False] * n
-    for i in range(n) :
-        if not visited[i] :
-            dfs(i, visited)
-            answer +=1
+    
+    count = 0 
+    for j in range(n) :
+        if not visited[j] :
+            queue = deque()
+            queue.append(j)
+            visited[j] = True
 
-    return answer
+            while queue :
+                q = queue.popleft()
+                for i, c in enumerate(computers[q]) :
+                    if not visited[i] and c == 1 :
+                        queue.append(i)                
+                        visited[i] = True
+            count += 1
+            
+    return count
+    
 
-# 제한사항
-# 컴퓨터의 개수 n은 1 이상 200 이하인 자연수입니다.
-# 각 컴퓨터는 0부터 n-1인 정수로 표현합니다.
-# i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computers[i][j]를 1로 표현합니다.
-# computer[i][i]는 항상 1입니다.
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])) # 2
-print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]	)) # 1
+solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
