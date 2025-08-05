5783c8305ca557449187134cce093f9506ba081b
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-08-05 14:11:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.03 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,25 @@
 from collections import deque
 
-
 def solution(maps):
-    def bfs(maps, pos):
-        visitied = [[False] * len(maps[0]) for _ in range(len(maps))]
-        visitied[pos[0]][pos[1]] = True
+    visitied = [[False] * len(maps[0]) for _ in range(len(maps))]
+    queue = deque([(0,0,1)])
+    visitied[0][0] = True
 
-        queue = deque([[pos[0], pos[1], 0]])
-        while queue:
-            x, y, _sum = queue.popleft()
-            _sum += 1
+    while queue:
+        x, y, sum = queue.popleft()
+        if x == (len(maps[0]) - 1) and y == (len(maps) - 1):
+            return sum
 
-            if y == len(maps)-1 and x == len(maps[0])-1:
-                return _sum
-
-            if x - 1 >= 0 and not visitied[y][x - 1] and maps[y][x-1] == 1:
-                queue.append([x - 1, y, _sum])
-                visitied[y][x - 1] = True
-            if x + 1 < len(maps[0]) and not visitied[y][x + 1] and maps[y][x+1] == 1:
-                queue.append([x + 1, y, _sum])
-                visitied[y][x + 1] = True
-            if y - 1 >= 0 and not visitied[y - 1][x] and maps[y-1][x] == 1:
-                queue.append([x, y - 1, _sum])
-                visitied[y - 1][x] = True
-            if y + 1 < len(maps) and not visitied[y + 1][x] and maps[y+1][x] == 1:
-                queue.append([x, y + 1, _sum])
-                visitied[y + 1][x] = True
-
-    result = bfs(maps, (0, 0))
-    return result if result else -1
+        for _x, _y in [(-1,0), (1,0), (0,-1), (0,1)] :
+            new_x = x+_x
+            new_y = y+_y
+            if new_x >= 0 and new_x <len(maps[0]) and new_y >=0 and new_y <len(maps) and maps[new_y][new_x] == 1 :
+                if not visitied[new_y][new_x] :
+                    v = new_x, new_y, sum+1
+                    queue.append(v)
+                visitied[new_y][new_x] = True
 
 
-maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
-print(solution(maps))
+    return -1
+
+print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
