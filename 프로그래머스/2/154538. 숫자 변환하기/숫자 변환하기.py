9734266c7afe1022619e18ec94a9d160a4d1960b
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-10-13 13:23:58 +0900

    [level 2] Title: 숫자 변환하기, Time: 528.30 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,24 +1,31 @@
 from collections import deque
+# x -> y
+# 방법 3가지
+# 1. x+n
+# 2. x*2
+# 3. x*3
 
-def solution(x, y, n):
-    queue = deque([[x, 0]])
-    visited = set([x])
-    
+# x, y, n 이 주어질 떄 최소 연산 회수 return, 없으면 -1
+
+def solution(x, y, n) :
+    queue = deque()
+    queue.append([0, x]) # step, value
+    visited = set({x})
+
     while queue :
-        s, count = queue.popleft()
-        if s == y : 
-            return count
-        
-        if s + n <= y and s + n not in visited:
-            queue.append([s+n, count+1])
-            visited.add(s+n)
-        if s * 2 <= y and s * 2 not in visited:
-            queue.append([s*2, count+1])
-            visited.add(s*2)
-        if s * 3 <= y and s * 3 not in visited:
-            queue.append([s*3, count+1])
-            visited.add(s*3)
+        step, px = queue.popleft()
+        if px == y:
+            return step
+        for action in [n, px, 2*px] :
+            nx = px+action
+            if nx not in visited and nx <= y:
+                queue.append([step+1, nx])
+                visited.add(nx)
+
     return -1
 
+print(solution(10,40,5)) # 2
+print(solution(10,40,30)) # 1
+print(solution(2,5,4)) # -1
 
-solution(10, 40, 5)
+
