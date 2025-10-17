739c606d0552d2e78744874c551780ad79dd1a9b
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-10-17 11:44:58 +0900

    [level 3] Title: N으로 표현, Time: 24.34 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,27 +1,29 @@
-def solution(N, number):
-    answer = 0
+# 예제
+# 5와 사칙연산 만으로 12를 표현
+# 12 = 5 + 5 + (5 / 5) + (5 / 5)
+# 12 = 55 / 5 + 5 / 5
+# 12 = (55 + 5) / 5
 
-    dp = [set() for _ in range(9)]
-    dp[1].add(N)
+# N, number가 주어질 떄, N과 사칙연산만 사용해서 표현할 수 있는 방법 중 최소값을 return
+# 나누기에서 나머지는 무시
+# 최소값이 8보다 크면 -1 retrun
+def solution(N, number) :
+    DP = [set() for _ in range(8)]
+    for i in range(8) :
+        DP[i].add(int(str(N)*(i+1)))
+        for j in range(i) :
+            for x in DP[j] :
+                for y in DP[i-j-1] :
+                    DP[i].add(x+y)
+                    DP[i].add(x-y)
+                    DP[i].add(x*y)
+                    if y!= 0 :
+                        DP[i].add(x//y)
 
-    for i in range(2, 9) :
-        dp[i].add(int(str(N)*i))
-
-        for j in range(1, i) :
-            for x in dp[j] :
-                for y in dp[i-j] :
-                    dp[i].add(x+y)
-                    dp[i].add(x-y)
-                    dp[i].add(x*y)
-                    if y != 0 :
-                        dp[i].add(x//y)
-
-    for i in range(len(dp)) :
-        if number in dp[i]:
-            return i
-
+        if number in DP[i] :
+            return i+1
     return -1
 
-
-print(solution(5, 12))
-print(solution(2, 11))
+N = 5 # 1~9
+number = 12 # 1~32,000
+print(solution(N, number)) # 4
