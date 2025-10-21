43e29d17050dedb1010f7a47bee3596b333b7c2e
kyunghoRyu <73271891+kyungho-ryu@users.noreply.github.com>
2025-10-21 11:20:53 +0900

    [level 3] Title: 단어 변환, Time: 0.67 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,36 +1,52 @@
 from collections import deque
+# begin에서 target으로 변환하는 가장 짧은 변환
+# 1. 한 번에 한 개의 알파벳만 바꿀 수 있습니다.
+# 2. words에 있는 단어로만 변환할 수 있습니다.
 
-def check_change(v1, v2) :
-    count = 0
-    for i in range(len(v1)) :
-        if v1[i] != v2[i] : count +=1
+# 예를 들어 begin이 "hit", target가 "cog"
+# words가 ["hot","dot","dog","lot","log","cog"]라면
+# "hit" -> "hot" -> "dot" -> "dog" -> "cog"와 같이 4단계를 거쳐 변환할 수 있습니다.
 
-    return True if count ==1 else False
+# 단어 길이: 3~10
+begin = "hit"
+target = "cog"
+words = ["hot", "dot", "dog", "lot", "log"]	 # 3개 이상 50개 이하
 
+# 500
+# 490
+# 480
+# 1
+def solution(begin, target, words) :
+    set_words = set(words)
 
-def solution(begin, target, words):
-    visited = {word : False for word in words}
-    visited[begin] = False
-    que = deque([(begin,0)])
-    while que :
-        item, count = que.popleft()
-        visited[item] = True
+    # 변환할 수없으면 0 return
+    if target not in set_words : return 0
 
-        if item == target :
-            return count
+    length = len(target)
 
-        for word in words :
-            if check_change(item, word) and not visited[word]:
-                que.append((word, count+1))
+    candidates = [[] for _ in range(length)]
+    for word in words :
+        for i, w in enumerate(word) :
+            if w not in candidates[i] :
+                candidates[i].append(w)
 
-    return 0
+    queue = deque()
+    queue.append([0, begin])
+    visited = set()
+    visited.add(begin)
 
-# 제한사항
-# 각 단어는 알파벳 소문자로만 이루어져 있습니다.
-# 각 단어의 길이는 3 이상 10 이하이며 모든 단어의 길이는 같습니다.
-# words에는 3개 이상 50개 이하의 단어가 있으며 중복되는 단어는 없습니다.
-# begin과 target은 같지 않습니다.
-# 변환할 수 없는 경우에는 0를 return 합니다.
+    while queue :
+        num, c_word = queue.popleft()
+        if c_word == target :
+            return num
 
-print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"])) # 4
-print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"])) # 0
+        for i in range(length) :
+            for candiate in candidates[i] :
+                if candiate != c_word[i] :
+                    n_word = c_word[0:i] + candiate + c_word[i+1:length]
+                    if n_word not in visited and n_word in set_words:
+                        queue.append([num+1, n_word])
+                        visited.add(n_word)
+    return -1
+
+print(solution(begin, target, words))
